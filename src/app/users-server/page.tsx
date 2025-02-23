5003ef331fbdb6d9130a814a3b2ec869389e0d74
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-23 23:34:41 +0300

    await response json and clg for testing

--- src/app/users-server/page.tsx
+++ src/app/users-server/page.tsx
@@ -8,4 +8,6 @@ type User = {
 
 export default async function UsersServer() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+  console.log(users);
 }
