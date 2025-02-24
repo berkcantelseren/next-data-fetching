4cbd56f1942e2dd9b208c6b2b100b58ca6b4ca8f
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-24 21:40:05 +0300

    add function for albums and posts

--- src/app/user-parallel/[id]/page.tsx
+++ src/app/user-parallel/[id]/page.tsx
@@ -10,3 +10,21 @@ type Album = {
   id: number;
   title: string;
 };
+
+async function getUsersPosts(userId: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+  return res.json();
+}
+
+async function getUserAlbums(userId: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+  );
+  return res.json();
+}
+
+export default async function UserProfile() {}
