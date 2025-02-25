50cf43c1c878da28c65fe5a6f36e5199d7e7ed7b
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-25 21:21:36 +0300

    add updateProduct , addProduct , addProducts etc in db

--- src/prisma-db.ts
+++ src/prisma-db.ts
@@ -16,3 +16,56 @@ const seedProducts = async () => {
 
 // Run seed if needed
 seedProducts();
+
+export async function getProducts(query?: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  if (query) {
+    return prisma.product.findMany({
+      where: {
+        OR: [
+          { title: { contains: query } },
+          { description: { contains: query } },
+        ],
+      },
+    });
+  }
+  return prisma.product.findMany();
+}
+
+export async function getProduct(id: number) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  return prisma.product.findUnique({
+    where: { id },
+  });
+}
+
+export async function addProduct(
+  title: string,
+  price: number,
+  description: string
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  return prisma.product.create({
+    data: { title, price, description },
+  });
+}
+
+export async function updateProduct(
+  id: number,
+  title: string,
+  price: number,
+  description: string
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  return prisma.product.update({
+    where: { id },
+    data: { title, price, description },
+  });
+}
+
+export async function deleteProduct(id: number) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  return prisma.product.delete({
+    where: { id },
+  });
+}
